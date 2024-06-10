8d45dce82b67d8f3b924417b3bbdf81474490f96
krisvananta <afifkrisvananta@gmail.com>
2024-06-10 08:16:33 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.cashifyupdate {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.cashifyupdate to javafx.fxml;
+    exports org.example.cashifyupdate;
+}
